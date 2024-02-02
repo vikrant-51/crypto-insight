3fde35f65bbf070b242121c6be33dbd78ead04e7
vikrant-51 <vikrantpathania59@gmail.com>
2024-02-02 15:30:59 +0530

    Deleted other unused reducers

--- src/redux/coinsReducer.js
+++
@@ -1,8 +0,0 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-const getCoins = createAction("getCoins");
-
-export const coinsReducer = createReducer({}, (builder) => {
-  builder
-    .addCase(getCoins, (state, action) => {})
-});
--- src/redux/exchangesReducer.js
+++
@@ -1,5 +0,0 @@
-import {createReducer } from "@reduxjs/toolkit";
-
-
-
-export const exchangesReducer = createReducer({}, () => {});
